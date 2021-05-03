2a1d86a9bb50e8e605e374daed6d73603a743a08
Amer Kovacevic <79953503+amerkovacevic@users.noreply.github.com>
2021-05-02 23:56:53 -0500

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      here
+    <div className="App">
+          here
+          {/* Header */}
+          {/* Home */}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCSOmWkPbsUu2kRu2pRkaQwuHcuHDc1_sk",
+  authDomain: "fir-a480b.firebaseapp.com",
+  projectId: "fir-a480b",
+  storageBucket: "fir-a480b.appspot.com",
+  messagingSenderId: "221028203652",
+  appId: "1:221028203652:web:74d2282349105d9b5b56d1",
+  measurementId: "G-KSY0BBZZ6Z"
+};

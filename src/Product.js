bcc417f5bd0b86c15b52ecb9504d45633ac7b4ce
Amer Kovacevic <79953503+amerkovacevic@users.noreply.github.com>
2021-05-03 03:21:59 -0500

    added functionality of add to cart button

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import "./Product.css"
 import StarIcon from '@material-ui/icons/Star';
+import { useStateValue } from './StateProvider';
 
 function Product({id, title, price, rating, image}) {
+    
+    const [{ basket }, dispatch] = useStateValue();
+
+    console.log('basket')
+    
+    const addToBasket = () => {
+        //put item in data layer
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                image: image,
+                price: price,
+                rating: rating,
+            },
+        });
+    };
+    
     return (
         <div className="product">
             <div className="product__info">
@@ -25,7 +45,7 @@ function Product({id, title, price, rating, image}) {
             alt=""
             />
 
-            <button>
+            <button onClick={addToBasket}>
                 Add to basket
             </button>
         </div>
